58215c72c397044fdcb6b19e3147834fd5bb0155
Raj Beemi <beemi.raja@gmail.com>
2022-09-18 23:42:46 +0100

    feat: fisrt commit jmeter master and worker images

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jmeter-master-worker-docker-images
 JMeter Master and Worker Docker Images using Base images
+
+https://github.com/beemi/jmeter-docker-base-image
---
+++ jmeter-worker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export JVM_ARGS="-Xmn4G -Xms4G -Xmx4g"
+echo "JVM_ARGS=${JVM_ARGS}"
+
+containerIp=$(hostname -i)
+
+echo "Test started on JMeter worker ${containerIp} at $(date +%T)"
+
+jmeter-server \
+  -JGmode=asych \
+  -JGasynch.batch.queue.size=200 \
+  -Dserver.rmi.ssl.disable=true \
+  -Dserver.rmi.localport=50000 \
+  -Dserver_port=1099 \
+  -Dprometheus.ip=0.0.0.0
