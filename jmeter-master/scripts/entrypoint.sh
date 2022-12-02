3834d38dee1b6218df98c9fc735485793b94237f
Raj Beemi <beemi.raja@gmail.com>
2022-12-02 00:34:50 +0000

    feat: updated master & worker image for k8

---
+++ jmeter-master/scripts/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+run_file_if_present() {
+  if [ -f $1 ]
+  then
+    echo "Running $1..."
+    $1
+  fi
+}
+
+on_exit() {
+  TEST_FAILED=$?
+  run_file_if_present /opt/dax/config/exit.sh
+  [ $TEST_FAILED -eq 0 ] && exit
+  echo "Load test failed. Exiting..."
+  exit 1
+}
+trap on_exit 0
+
+echo "Starting load test..."
+
+export JVM_ARGS="-Xmn512m -Xms512m -Xmx512m"
+echo "JVM_ARGS=${JVM_ARGS}"
+
+run_file_if_present /opt/jaitechltd/config/pre-test.sh
+run_file_if_present /opt/dax/jaitechltd/test.sh
+run_file_if_present /opt/dax/jaitechltd/post-test.sh
+
+echo "Finished load test"
