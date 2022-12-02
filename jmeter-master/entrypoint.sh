e5dea3dd170859eb86ccfa91ab7740d861afd842
Raj Beemi <beemi.raja@gmail.com>
2022-12-02 15:13:57 +0000

    feat: jmeter master entry point path updated

--- jmeter-master/entrypoint.sh
+++ jmeter-master/entrypoint.sh
@@ -17,7 +17,10 @@ on_exit() {
 }
 trap on_exit 0
 
-echo "Starting load test..."
+hostIp=$(hostname -i)
+dateTime=$(date +%Y-%m-%d-%H-%M-%S)
+
+echo "Starting load test from master... [${hostIp}] on ${dateTime}"
 
 export JVM_ARGS="-Xmn512m -Xms512m -Xmx512m"
 echo "JVM_ARGS=${JVM_ARGS}"
