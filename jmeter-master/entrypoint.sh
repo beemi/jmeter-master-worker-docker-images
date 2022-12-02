e352638dba949996e6129adfd239c84cebb32d07
Raj Beemi <beemi.raja@gmail.com>
2022-12-02 14:04:23 +0000

    feat: jmeter master image updated

--- jmeter-master/entrypoint.sh
+++ jmeter-master/entrypoint.sh
@@ -22,8 +22,8 @@ echo "Starting load test..."
 export JVM_ARGS="-Xmn512m -Xms512m -Xmx512m"
 echo "JVM_ARGS=${JVM_ARGS}"
 
-run_file_if_present /opt/jaitechltd/config/pre-test.sh
-run_file_if_present /opt/dax/jaitechltd/test.sh
-run_file_if_present /opt/dax/jaitechltd/post-test.sh
+#run_file_if_present /opt/jaitechltd/config/pre-test.sh
+run_file_if_present /opt/dax/jaitechltd/config/test.sh
+#run_file_if_present /opt/dax/jaitechltd/config/post-test.sh
 
 echo "Finished load test"
